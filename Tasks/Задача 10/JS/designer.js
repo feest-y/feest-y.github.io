ac0ba5bdf16d5858d4a835df83f11bd0c6c65157
feest-y <62559068+feest-y@users.noreply.github.com>
2020-03-23 18:06:07 +0200

    Revert "enterprise"
    This reverts commit c8a5c13431cf1824bbc65e49f1a288795af00249.

--- Tasks/Задача 10/JS/designer.js
+++
@@ -1,5 +0,0 @@
-var h, zp;
-h = parseInt(prompt("Кількість напрацьованих годин:", h));
-zp = h * 85;
-document.write("<p>", "Ваша зарплата - ", zp, "UAH",
-    "</p>");
--- Tasks/Задача 10/JS/teacher.js
+++
@@ -1,5 +0,0 @@
-var h, zp;
-h = parseInt(prompt("Кількість напрацьованих годин:", h));
-zp = h * 40;
-document.write("<p>", "Ваша зарплата - ", zp, "UAH",
-    "</p>");
